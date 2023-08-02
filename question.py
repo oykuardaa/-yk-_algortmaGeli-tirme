b2983ff3642dedb7e33604071e1dd1afde1eacc7
oykuardaa <134720516+oykuardaa@users.noreply.github.com>
2023-08-03 02:33:36 +0300

    Update question.py

--- question.py
+++ question.py
@@ -1,1 +1,24 @@
 print("Hello, world!")
+def longest_substring_without_duplicates(s):
+    start = 0
+    max_len = 0
+    max_substring = ""
+    char_to_index = {}
+
+    for i, char in enumerate(s):
+        if char in char_to_index and char_to_index[char] >= start:
+            start = char_to_index[char] + 1
+
+        char_to_index[char] = i
+
+        if i - start + 1 > max_len:
+            max_len = i - start + 1
+            max_substring = s[start:i + 1]
+
+    return max_substring, max_len
+
+
+if __name__ == "__main__":
+    input_str = input("input: ")
+    substring, length = longest_substring_without_duplicates(input_str)
+    print(f"output: {substring} length: {length}")
